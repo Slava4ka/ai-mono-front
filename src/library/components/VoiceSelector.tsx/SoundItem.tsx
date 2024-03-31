0230579228c3faa3a264c6648a4a44feb57ffe88
Vyacheslav Osepyan <mr-93@mail.ru>
2024-03-31 12:07:06 +0300

    Add voice examples

--- src/library/components/VoiceSelector.tsx/SoundItem.tsx
+++ src/library/components/VoiceSelector.tsx/SoundItem.tsx
@@ -1,45 +1,73 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
-import { Stack, Typography, IconButton } from '@mui/material';
+import {
+	Stack, Typography, IconButton, Box,
+} from '@mui/material';
 
 interface IProps {
-    isSelected: boolean;
+	audioUrl: string;
+	isPlaying: boolean;
+	togglePlay(): void;
     title: string;
+    isSelected: boolean;
     onClick(): void;
 }
 
-const SoundItem: React.FunctionComponent<IProps> = ({ isSelected, onClick, title }) => (
-	<Stack
-		onClick={onClick}
-		direction="row"
-		px="14px"
-		py="10px"
-		alignItems="center"
-		justifyContent="space-between"
-		sx={{
-			borderRadius: 4,
-			borderColor: '#DBDBDD',
-			borderStyle: 'solid',
-			borderWidth: '4px',
-			cursor: 'pointer',
+const SoundItem: React.FunctionComponent<IProps> = ({
+	isSelected, audioUrl, onClick, title, isPlaying, togglePlay,
+}) => {
+	const audioPlayer = useRef<HTMLAudioElement>(null);
 
-			'&:hover': {
-				borderColor: '#4788EAB9',
-			},
+	useEffect(() => {
+		if (!isPlaying && (audioPlayer.current?.played.length ?? 0) > 0) {
+			audioPlayer.current?.pause();
+		} else if (isPlaying) {
+			audioPlayer.current?.play();
+		}
+	}, [isPlaying]);
 
-			...(isSelected && {
-				borderColor: '#4788EA',
-			}),
+	return (
+		<Stack direction="row" spacing={2} alignItems="center">
+			<Box
+				onClick={onClick}
+				px="14px"
+				py="10px"
+				display="flex"
+				alignItems="center"
+				justifyContent="space-between"
+				flex={1}
+				sx={{
+					borderRadius: 4,
+					borderColor: '#DBDBDD',
+					borderStyle: 'solid',
+					borderWidth: '4px',
+					cursor: 'pointer',
 
-		}}
-	>
-		<Typography variant="body1" sx={{ color: '#000010', fontSize: 14, fontWeight: '600' }}>{title}</Typography>
+					'&:hover': {
+						borderColor: '#4788EAB9',
+					},
 
-		<IconButton sx={{ background: '#DBDBDD' }} size="small">
-			{false ? <PauseIcon sx={{ height: 24, width: 24 }} /> : <PlayArrowIcon sx={{ height: 24, width: 24 }} />}
-		</IconButton>
-	</Stack>
-);
+					...(isSelected && {
+						borderColor: '#4788EA',
+					}),
+
+				}}
+			>
+				<Typography variant="body1" sx={{ color: '#000010', fontSize: 14, fontWeight: '600' }}>{title}</Typography>
+			</Box>
+			<Box>
+				<audio ref={audioPlayer} onEnded={togglePlay}>
+					<source src={audioUrl} type="audio/wav" />
+					<track kind="captions" src="your-captions-file.vtt" srcLang="ru" />
+					Your browser does not support the audio element.
+				</audio>
+				<IconButton sx={{ background: '#DBDBDD' }} size="small" onClick={togglePlay}>
+					{isPlaying ? <PauseIcon sx={{ height: 24, width: 24 }} /> : <PlayArrowIcon sx={{ height: 24, width: 24 }} />}
+				</IconButton>
+			</Box>
+		</Stack>
+	);
+};
 
 export default SoundItem;
--- src/library/components/VoiceSelector.tsx/index.tsx
+++ src/library/components/VoiceSelector.tsx/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Stack, SxProps, Theme, Typography,
 } from '@mui/material';
@@ -6,6 +6,9 @@ import GENDER from 'library/consts/Gender';
 import LANGUAGE from 'library/consts/Language';
 import ToggleButton from '../ToggleButton';
 import SoundItem from './SoundItem';
+
+import femaleVoice from '../../../resources/audio/female.wav';
+import maleVoice from '../../../resources/audio/male.wav';
 
 interface IProps {
 	language: LANGUAGE | undefined;
@@ -26,34 +29,54 @@ const LANGUAGE_BUTTON_STYLE: SxProps<Theme> = {
 
 const VoiceSelector: React.FunctionComponent<IProps> = ({
 	language, gender, handleSelectLanguage, handleGenderLanguage,
-}) => (
-	<Stack direction="column" spacing={2} pr="20%">
-		<Typography variant="body1" sx={{ color: '#6E7B87', fontWeight: 'bold' }}>Настройки голоса</Typography>
-		<Stack direction="row" spacing={1}>
-			<ToggleButton
-				isSelected={language === LANGUAGE.RUSSIAN}
-				onClick={() => handleSelectLanguage(LANGUAGE.RUSSIAN)}
-				sx={LANGUAGE_BUTTON_STYLE}
-				selectedBackground="#4788EA"
-				selectedColor="#FFF"
-			>
-				Русский
-			</ToggleButton>
-			<ToggleButton
-				isSelected={language === LANGUAGE.ENGLISH}
-				onClick={() => handleSelectLanguage(LANGUAGE.ENGLISH)}
-				sx={LANGUAGE_BUTTON_STYLE}
-				selectedBackground="#4788EA"
-				selectedColor="#FFF"
-			>
-				Английский
-			</ToggleButton>
+}) => {
+	const [playingTrack, setPlayingTrack] = useState<GENDER>();
+
+	const handleTogglePlay = (trackName: GENDER) => setPlayingTrack((prev) => (trackName !== prev ? trackName : undefined));
+
+	return (
+		<Stack direction="column" spacing={2}>
+			<Typography variant="body1" sx={{ color: '#6E7B87', fontWeight: 'bold' }}>Настройки голоса</Typography>
+			<Stack direction="row" spacing={1}>
+				<ToggleButton
+					isSelected={language === LANGUAGE.RUSSIAN}
+					onClick={() => handleSelectLanguage(LANGUAGE.RUSSIAN)}
+					sx={LANGUAGE_BUTTON_STYLE}
+					selectedBackground="#4788EA"
+					selectedColor="#FFF"
+				>
+					Русский
+				</ToggleButton>
+				<ToggleButton
+					isSelected={language === LANGUAGE.ENGLISH}
+					onClick={() => handleSelectLanguage(LANGUAGE.ENGLISH)}
+					sx={LANGUAGE_BUTTON_STYLE}
+					selectedBackground="#4788EA"
+					selectedColor="#FFF"
+				>
+					Английский
+				</ToggleButton>
+			</Stack>
+
+			<SoundItem
+				title="Женский"
+				audioUrl={femaleVoice}
+				isPlaying={playingTrack === GENDER.FEMALE}
+				isSelected={gender === GENDER.FEMALE}
+				onClick={() => handleGenderLanguage(GENDER.FEMALE)}
+				togglePlay={() => handleTogglePlay(GENDER.FEMALE)}
+			/>
+
+			<SoundItem
+				title="Мужской"
+				audioUrl={maleVoice}
+				isPlaying={playingTrack === GENDER.MALE}
+				isSelected={gender === GENDER.MALE}
+				onClick={() => handleGenderLanguage(GENDER.MALE)}
+				togglePlay={() => handleTogglePlay(GENDER.MALE)}
+			/>
 		</Stack>
-
-		<SoundItem title="Женский" isSelected={gender === GENDER.FEMALE} onClick={() => handleGenderLanguage(GENDER.FEMALE)} />
-
-		<SoundItem title="Мужской" isSelected={gender === GENDER.MALE} onClick={() => handleGenderLanguage(GENDER.MALE)} />
-	</Stack>
-);
+	);
+};
 
 export default VoiceSelector;
